cfcde52f4dbc04004c66f3fd961b052610d3fe27
Jessica Kellogg <jmy.kellogg@gmail.com>
2024-11-22 16:59:00 -0800

    add populate data template

--- django_app/admin.py
+++ django_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Unit
+from .models import Unit, Ingredient, Conversion
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
@@ -7,4 +7,14 @@ class UnitAdmin(admin.ModelAdmin):
     ordering = ['name', 'abbreviation', 'display_name', 'system']
     search_fields = ['name', 'abbreviation', 'display_name', 'system']
      
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_name')
+    ordering = ['name', 'display_name']
+    search_fields = ['name', 'display_name']
 
+@admin.register(Conversion)
+class ConversionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ingredient', 'from_unit', 'to_unit', 'factor')
+    ordering = ['id', 'ingredient', 'from_unit', 'to_unit', 'factor']
+    search_fields = ['id', 'ingredient', 'from_unit', 'to_unit', 'factor']
--- django_app/models.py
+++ django_app/models.py
@@ -5,7 +5,7 @@ class Unit(models.Model):
       ('imperial', 'Imperial'),
       ('metric', 'Metric'),
     ]
-
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True, blank=False, null=False)
     display_name = models.CharField(max_length=50)
     abbreviation = models.CharField(max_length=25)
@@ -13,3 +13,21 @@ class Unit(models.Model):
 
     def __str__(self):
       return self.name
+
+class Ingredient(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    display_name = models.CharField(max_length=50)
+
+    def __str__(self):
+      return self.name
+
+class Conversion(models.Model):
+    id = models.AutoField(primary_key=True)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    from_unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='from_unit')
+    to_unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='to_unit')
+    factor = models.DecimalField(max_digits=10, decimal_places=5)
+
+    def __str__(self):
+      return f"{self.from_unit} to {self.to_unit} conversion factor: {self.factor}"
