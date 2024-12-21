bbff15fe2f9f0d71d4540d76e13b8d5072bf2d7a
Jessica Kellogg <jmy.kellogg@gmail.com>
2024-12-20 17:38:08 -0800

    add navbar

--- next_app/src/components/layout.tsx
+++ next_app/src/components/layout.tsx
@@ -5,13 +5,16 @@ export default function Layout({ children }) {
   return (
     <>
       <nav className="p-4 bg-gray-800 text-white">
-        <Link href="/">
-          <Image
-            src="/static/recipe_book.jpg"
-            alt="Recipe Book Icon"
-            width={150}
-            height={150}
-          ></Image>
+        <Link className="flex items-center" href="/">
+          <div className="overflow-hidden rounded-full w-fit">
+            <Image
+              src="/static/recipe_logo.jpg"
+              alt="Recipe Book Icon"
+              width={50}
+              height={50}
+            ></Image>
+          </div>
+          <h1 className="inline-block ml-2 text-2xl font-bold">Recipe Book</h1>
         </Link>
       </nav>
       <main>{children}</main>
